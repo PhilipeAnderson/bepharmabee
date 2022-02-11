dba2cae1d97233ba5737f308a4c6d5e22e750295
philipe_kraken <philipeacampos@hotmail.com>
2022-02-11 18:18:48 -0300

    refactored theme { frontend }

--- app/App.tsx
+++ app/App.tsx
@@ -5,8 +5,9 @@ import theme from './src/global/styles/theme';
 import { Dashboard } from './src/pages/Dashboard';
 
 export default function App() {
+  
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={ theme }>
       <Dashboard />
     </ThemeProvider>
   );
