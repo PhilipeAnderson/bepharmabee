200b5a471e10db5eb2e659c4e482d997097b3f55
philipe_kraken <philipeacampos@hotmail.com>
2022-02-14 17:03:19 -0300

    Add react navigation buttom tabs

--- app/App.tsx
+++ app/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './src/global/styles/theme';
-import { Login } from './src/pages/Login';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   
   return (
     <ThemeProvider theme={ theme }>
-      <Login />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
---
+++ app/src/routes/app.routes.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { MaterialIcons, FontAwesome, Fontisto } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+import { Dashboard } from '../pages/Dashboard';
+import { Login } from '../pages/Login';
+
+
+export function AppRoutes() {
+
+  const theme = useTheme();
+
+  return(
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.secondary,
+        tabBarStyle: {
+          height: 60,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 10,
+          backgroundColor: theme.colors.backgroundDark 
+        }  
+      }}
+    >
+      <Screen 
+        name="Home"
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <MaterialIcons 
+            name="home"
+            size={size}
+            color={color}
+              />
+          )
+        }}
+      />
+      <Screen 
+        name="Especiais"
+        component={Login}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <FontAwesome
+              name="wheelchair"
+              size={size}
+              color={color}
+            />  
+          )
+        }}
+      />
+      <Screen 
+        name="Vacinas"
+        component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) => 
+            <Fontisto
+              name="injection-syringe"
+              size={size}
+              color={color}
+            />  
+          )
+        }}
+      />
+    </Navigator>
+  )
+};
