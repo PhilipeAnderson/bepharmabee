4423d336fdbbe3952d09fd08d210dbfec0b6e085
philipe_kraken <philipeacampos@hotmail.com>
2022-03-02 14:29:18 -0300

    Started Server for to send information from API/APP

--- node/index.js
+++ node/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-//const routes = require('./config/routes');
+const routes = require('./config/routes');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended}))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
 
-app.use('./routes');
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Started Ok');
--- node/server.js
+++ node/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./index');
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 const server = http.createServer(app);
 
