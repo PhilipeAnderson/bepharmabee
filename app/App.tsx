7baf5412d3b8bf0c44a208d168b0cb6b526cf02b
philipe_kraken <philipeacampos@hotmail.com>
2022-04-15 23:31:35 -0300

    Clear

--- app/App.tsx
+++ app/App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/global/styles/theme';
 
-import { AppRoutes } from './src/routes/app.routes';
 
 import { Splash } from './src/pages/Splash';
 import { Login } from './src/pages/Login';
@@ -12,6 +11,7 @@ import { Product } from './src/pages/Product';
 import { Specials } from './src/pages/Specials';
 import { Vaccine } from './src/pages/Vaccine';
 
+import { AppRoutes } from './src/routes/app.routes';
 import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
